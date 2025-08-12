dea9988a15647e310af62d5078d49469a9e9582f
prapulkrishna-shaik <shaikprapul4@gmail.com>
2025-08-12 10:55:38 +0530

    Fix: Set base path and use plugin-react-swc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cicd-jenkins /',
   plugins: [react()],
-})
+  base: '/cicd-jenkins/',
+});
